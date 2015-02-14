8e173cadc0b544e0d9e42f4d995d87d2122be87b
Savor d'Isavano <anohig_isavay@163.com>
2015-02-14 21:25:12 +0800

    Added new sections.

--- blog/urls.py
+++ blog/urls.py
@@ -12,12 +12,15 @@ Blog urls.
 __author__ = "Savor d'Isavano"
 
 from django.conf.urls import patterns, include, url
-from .views import IndexView, PostView, TagView
+from .views import PostListView, PostView, TagView
 
 urlpatterns = patterns(
     '',
 #    url('^(p(?P<page>))?$', IndexView.as_view(), name='index')
-    url('^$', IndexView.as_view(), name='index'),
+    url('^$', PostListView.as_view(), name='index'),
+    url('^fictional-world/$', PostListView.as_view(), name='fw'),
+    url('^conlang/$', PostListView.as_view(), name='cl'),
+    url('^programming/$', PostListView.as_view(), name='programming'),
     url('^tag/(?P<slug>.+)/$', TagView.as_view(), name='tag'),
-    url('^(?P<slug>.+)/$', PostView.as_view(), name='detail'),
+    url('^p/(?P<slug>.+)/$', PostView.as_view(), name='detail'),
 )
--- blog/views.py
+++ blog/views.py
@@ -12,21 +12,36 @@ Blog views.
 __author__ = "Savor d'Isavano"
 
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.core.paginator import (
     Paginator, EmptyPage, PageNotAnInteger
 )
+from django.db.models import Q
 from utils.paginator import make_page
 from .models import Post
 
 
-class IndexView(ListView):
+class IndexView(TemplateView):
+    template_name = 'blog/index.djhtml'
+
+
+class PostListView(ListView):
     template_name = 'blog/list.djhtml'
     model = Post
 
     def get_queryset(self):
-        post_list = Post.objects.order_by('-created').all()
+        query = self.request.GET.get('q')
         page = self.request.GET.get('page')
+        post_list = Post.objects.order_by('-created')
+        if query:
+            qs = query.split(' ')
+            for q in qs:
+                post_list = post_list.filter(
+                    Q(title__contains=q) |
+                    Q(subtitle__contains=q) |
+                    Q(content__contains=q)
+                )
+
         posts = make_page(post_list, per_page=5, current_page=page)
 
         return posts
