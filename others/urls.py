c28265f710dd8b5bb20b4c95abdc1570ea7a12e5
Savor d'Isavano <anohig_isavay@163.com>
2015-02-15 17:42:45 +0800

    Updated 正義の目.

--- others/urls.py
+++ others/urls.py
@@ -12,9 +12,11 @@ Others urls.
 __author__ = "Savor d'Isavano"
 
 from django.conf.urls import patterns, include, url
-from .views import IndexView
+from .views import IndexView, SeigiNoMeView, SeimeiToIshikiNoJyoudanView
 
 
 urlpatterns = patterns('',
     url('^$', IndexView.as_view(), name='index'),
+    url('^seiginome$', SeigiNoMeView.as_view(), name='seiginome'),
+    url('^seimeitoishikinojyoudan$', SeimeiToIshikiNoJyoudanView.as_view(), name='seimeitoishikinojyoudan'),
 )
--- others/views.py
+++ others/views.py
@@ -17,3 +17,11 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'others/index.djhtml'
+
+
+class SeigiNoMeView(TemplateView):
+    template_name = 'others/seigi_no_me.djhtml'
+
+
+class SeimeiToIshikiNoJyoudanView(TemplateView):
+    template_name = 'others/seimei_to_ishiki_no_jyoudan.djhtml'
